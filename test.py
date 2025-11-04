ad4e83a2491587b44f2e89774f62f96c7b0de6a5
Каневская Елизавета Юрьевна <lizok.kanev@mail.ru>
2025-11-04 18:04:35 +0300

    commit 7
    create file txt

--- test.py
+++ test.py
@@ -1,8 +1,6 @@
 import unittest
 from main import create_login, len_str, front_characters, back_characters, impossible_characters , valid_value , error_dog
-
-text = input("Введите почту : ")
-data=text.split()
+import unittest
 
 class MailTest(unittest.TestCase):
 
@@ -36,9 +34,43 @@ class RegularTest(unittest.TestCase):
         for text in data:
             assert create_login(text) is True
 
-if __name__ == "__main__":
-    suite = unittest.TestSuite()
-    suite.addTest(unittest.makeSuite(MailTest))
-    suite.addTest(unittest.makeSuite(RegularTest))
-    runner = unittest.TextTestRunner()
-    runner.run(suite)
+while True:
+    print("Если вы хотите проверить корректность почты через файл — введите 1")
+    print("Если вы хотите проверить корректность почты через ввод с консоли — введите 2")
+    print("Если вы хотите выйти — введите 0")
+
+    message = input("Введите выбор: ")
+
+    if message == "1":
+        # Проверка через файл
+        with open('testing_file.txt', 'r', encoding='utf-8') as f:
+            data = [line.strip() for line in f.readlines()]
+
+
+        suite = unittest.TestSuite()
+        suite.addTest(unittest.makeSuite(MailTest))
+        suite.addTest(unittest.makeSuite(RegularTest))
+        runner = unittest.TextTestRunner()
+        runner.run(suite)
+
+    elif message == "2":
+        # Проверка через консоль
+        text = input("Введите почту: ")
+        data = text.split()
+
+
+        suite = unittest.TestSuite()
+        suite.addTest(unittest.makeSuite(MailTest))
+        suite.addTest(unittest.makeSuite(RegularTest))
+        runner = unittest.TextTestRunner()
+        runner.run(suite)
+
+    elif message == "0":
+        print("Выход из программы.")
+        break
+
+    else:
+        print("Некорректный ввод. Попробуйте снова.")
+
+
+
